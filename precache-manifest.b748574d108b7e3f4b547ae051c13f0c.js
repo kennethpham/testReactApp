bdf40290a6efa2e48414281664df0933e3c3935d
Kenneth Pham <kennethpham98@gmail.com>
2020-08-03 13:35:14 -0700

    Updates

---
+++ precache-manifest.b748574d108b7e3f4b547ae051c13f0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008d48ea7c0ad84bb8a252b37602cee1",
+    "url": "/testReactApp/index.html"
+  },
+  {
+    "revision": "e3b4482d31a7e10fff42",
+    "url": "/testReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/testReactApp/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testReactApp/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b4482d31a7e10fff42",
+    "url": "/testReactApp/static/js/main.15138046.chunk.js"
+  },
+  {
+    "revision": "3c5057e38d247494a069",
+    "url": "/testReactApp/static/js/runtime-main.0d62bce3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
